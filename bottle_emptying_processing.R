e8abc4a63d874c96b3564e869a1e46cf0321d44f
GenghisKhandybar <william.medwid@gmail.com>
2022-02-02 14:25:32 -0800

    Improved wording, documentation, data validation.

--- bottle_emptying_processing.R
+++ bottle_emptying_processing.R
@@ -4,6 +4,8 @@ cp = read.csv("CPcoded.csv") # Originally "Coder Bottle Emptying Data" - put in 
 uom = read.csv("UOMcoded.csv") # Originally "Typical Feeding - Bottle Weight Amt and rate" - converted to CSV
 
 #University of Michigan's precise coding - adjusting the time points to match with our codes.
+#- neonur_bot1_pre: bottle weight before feeding
+#- neonur_bot1_post: bottle weight after feeding
 uom <-  uom %>% 
   mutate(Dyad_set = if_else(
     timepoint == 1, "2wkB",
@@ -22,8 +24,18 @@ combined <- cp %>% left_join(uom, by = c("id", "Dyad_set"))
 
 
 
-#Verifying distinct values
+#Verifying distinct values and data integrity
 combined %>% distinct(Bottle.Emptying)
 combined %>% distinct(Dyad_set)
+combined %>% #This plot reveals possible change in baseline bottle weight?
+  pivot_longer(cols = c(neonur_bot1_pre,
+                        neonur_bot2_pre,
+                        neonur_bot3_pre,
+                        neonur_bot1_post,
+                        neonur_bot2_post,
+                        neonur_bot3_post), values_to = "Weight", names_to = "bottle_num") %>% 
+  ggplot(aes(x = id, y = Weight, col = bottle_num)) +
+  geom_point() +
+  facet_grid(~Dyad_set)
 
 write.csv(combined, file = "combined_emptying.csv")
